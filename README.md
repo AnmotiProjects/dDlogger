eb2268be870816be31a5777cf3632e162e3f4b6a
anmoti <77442643+anmoti@users.noreply.github.com>
2023-01-09 20:20:05 +0900

    1.0.0

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# dlogger
+# dd-logger
 ```javascript
 dlogger.setDefault({
 
--- test.js
+++ test.js
@@ -1,12 +1,12 @@
-const dlogger = require("./lib/index.js");
+const DDlogger = require("./lib/index.js");
 
-dlogger.setDefault({
+DDlogger.setDefault({
     levels: ["error", "warn", "info"],
     timezone: 9, //JST = +9
     //timeFormat: "YYYY/MM/DD hh:mm:sssss"
 });
 
-const logger = new dlogger.Channel();
+const logger = new DDlogger.Channel();
 
 logger.info(`It is "Info"`);
 logger.warn(`It is "Warn"`);
